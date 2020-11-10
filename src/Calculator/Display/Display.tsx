2b0916e385c67210c7d59caf06fc8ce9f4302704
Arthur-Tolkachov <babanjerelo@gmail.com>
2020-11-10 15:03:12 +0200

    finish

--- src/Calculator/Display/Display.tsx
+++ src/Calculator/Display/Display.tsx
@@ -7,8 +7,10 @@ type PropsType = {
 
 const Display: React.FC<PropsType> = ({display}) => {
 
+    const fontSize = display.length <= 6 ? "5em" : display.length <= 11 ? "3em" : "1.5em"
+
     return (
-        <div className={`${s.display}`}>
+        <div className={`${s.display}`} style={{fontSize}}>
             {display}
         </div>
     )
